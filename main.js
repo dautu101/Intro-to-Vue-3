686e8ec49578daa7cfc8f36fe4384a51a847e8e2
TM <tuannguyendl31@gmail.com>
2022-01-15 21:44:08 +0700

    this is

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
+            product: 'DFT Marketplace',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester']
